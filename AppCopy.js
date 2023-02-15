1accae1f166cddbf785d9d76f7d31066235d9893
Naimul Mushfiq Mahim <naimulmushfiq98@gmail.com>
2023-02-15 14:40:10 +0600

    2nd commit wiht app.js

---
+++ AppCopy.js
@@ -0,0 +1,13 @@
+import ClickCounter from "./Components/clickCounter/clickCounter.js";
+import HoverCounter from "./Components/hoverCounter/hoverCounter";
+
+function App() {
+	return (
+		<div>
+			<ClickCounter></ClickCounter>
+			<HoverCounter></HoverCounter>
+		</div>
+	);
+}
+
+export default App;
